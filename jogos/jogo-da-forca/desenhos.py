83296d9c063f5f1a35187ead627b66f70491b231
ramons <ramon.santos@me.com.br>
2020-02-20 19:15:50 -0300

    one more step

---
+++ jogos/jogo-da-forca/desenhos.py
@@ -0,0 +1,67 @@
+class desenhos():
+
+    def ganhador():
+        print("Parabéns, você ganhou!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
+
+    def desenha_forca(erros):
+        print("  _______     ")
+        print(" |/      |    ")
+
+        if(erros == 1):
+            print(" |      (_)   ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+
+        if(erros == 2):
+            print(" |      (_)   ")
+            print(" |      \     ")
+            print(" |            ")
+            print(" |            ")
+
+        if(erros == 3):
+            print(" |      (_)   ")
+            print(" |      \|    ")
+            print(" |            ")
+            print(" |            ")
+
+        if(erros == 4):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |            ")
+            print(" |            ")
+
+        if(erros == 5):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |            ")
+
+        if(erros == 6):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      /     ")
+
+        if (erros == 7):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      / \   ")
+
+        print(" |            ")
+        print("_|___         ")
+        print()
+
+if __name__ == "__main__":
+    pass
--- jogos/jogo-da-forca/jogo-da-forca.py
+++ jogos/jogo-da-forca/jogo-da-forca.py
@@ -1,5 +1,5 @@
 import random
-
+import desenhos
 
 letras_descobertas = []
 letras_ja_utilizadas = ""
@@ -17,15 +17,13 @@ numero = random.randrange(0, len(palavras))
 palavra_secreta = palavras[numero].upper()
 
 
-print(palavra_secreta)
-
 print("\n*** Jogo da Forca ***\n")
 
 letras_descobertas = ["-" for letra in palavra_secreta]
 
 print (letras_descobertas)
 acertou = False
-
+erros = 0
 while acertou == False:
     print("A palavra tem %s letras" % len(palavra_secreta))
     print("Letra já utilizadas: %s " % letras_ja_utilizadas)
@@ -36,15 +34,19 @@ while acertou == False:
     for i in range(0, len(palavra_secreta)) :
         if letra == palavra_secreta[i] :
             letras_descobertas[i] = letra
-        print(letras_descobertas[i])
+            acertou = True
 
-    acertou = True
+    if acertou == False:
+        erros += 1
+        desenhos.desenhos.desenha_forca(erros)
+        
+    print(letras_descobertas[i])
 
     for x in range(0, len(letras_descobertas)) :
         if letras_descobertas[x] == "-" :
             acertou = False
     
-print("Parabéns! Você acertou!")
+desenhos.desenhos.ganhador()
 input()
 
 
